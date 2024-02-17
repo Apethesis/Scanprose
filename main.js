a4d561a4ec316569abf1fa6b22d149a99c1570e0
Nenad Blagovcanin <nenadblagovcanin2@gmail.com>
2024-02-17 01:12:33 +0100

    env setup

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1400,
-    height: 850,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    const win = new BrowserWindow({
+        width: 1400,
+        height: 850,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
 
     win.maximize();
-    win.loadFile('index.html');
-
+    win.loadURL('http://localhost:8000');
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
